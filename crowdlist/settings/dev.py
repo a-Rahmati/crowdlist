6f237eae958629a5f7a91e88b5fd03c16bb00f25
Amirhosein Rahmati <amirhoseinrahmati@yandex.com>
2024-12-05 18:12:21 +0330

    Add wagtail files

---
+++ crowdlist/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-zai#5d+&r3kn7(e@nzr%tzart6uq1#z16c1&tomd*5z30lo0%g"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
